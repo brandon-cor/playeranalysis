942d2a43e69fe1d17391ab618701db968a429347
Brandon <15brandon51@gmail.com>
2022-12-24 19:27:39 -0800

    actually made the edits from the previous commit

--- center-defensive-mid.py
+++ center-defensive-mid.py
@@ -1,42 +1,25 @@
-# For Positions - LCMF, LCMF3, LDMF, DMF, RCMF, RCMF3, RDMF
-
 import csv
-list = []
-with open('Player_A_Diop.csv', encoding="utf-8") as f:q
-    csv_reader = csv.reader(f)
+
+positions = ['LCMF', 'LCMF3', 'LDMF', 'DMF', 'RCMF', 'RCMF3', 'RDMF']
+
+with open('Player_A_Diop.csv', encoding="utf-8") as f_in, open('Player_A_Diop2.csv', 'w', encoding="utf8", newline='') as f_out:
+    csv_reader = csv.reader(f_in)
+    writer = csv.writer(f_out)
+
     for line in csv_reader:
-        # print(line)
-        if 'LCMF' in line[3]:
+        if line[3] in positions:
+            # checks for midfielder's stats: goals, assists, completed passes
             line[31] = ((float(line[6])/float(line[5])) + float(line[8]) + (float(line[10])/float(line[9])) + (
                 float(line[19])/float(line[18])) + (float(line[13])/float(line[12])) + (float(line[28])/float(line[27]))/6)
-
-        if 'LCMF3' in line[3]:
-            line[31] = ((float(line[6])/float(line[5])) + float(line[8]) + (float(line[10])/float(line[9])) + (
-                float(line[19])/float(line[18])) + (float(line[13])/float(line[12])) + (float(line[28])/float(line[27]))/6)
-
-        if 'LDMF' in line[3]:
-            line[31] = ((float(line[6])/float(line[5])) + float(line[8]) + (float(line[10])/float(line[9])) + (
-                float(line[19])/float(line[18])) + (float(line[13])/float(line[12])) + (float(line[28])/float(line[27]))/6)
-
-        if 'DMF' in line[3]:
-            line[31] = ((float(line[6])/float(line[5])) + float(line[8]) + (float(line[10])/float(line[9])) + (
-                float(line[19])/float(line[18])) + (float(line[13])/float(line[12])) + (float(line[28])/float(line[27]))/6)
-
-        if 'RCMF' in line[3]:
-            line[31] = ((float(line[6])/float(line[5])) + float(line[8]) + (float(line[10])/float(line[9])) + (
-                float(line[19])/float(line[18])) + (float(line[13])/float(line[12])) + (float(line[28])/float(line[27]))/6)
-
-        if 'RCMF3' in line[3]:
-            line[31] = ((float(line[6])/float(line[5])) + float(line[8]) + (float(line[10])/float(line[9])) + (
-                float(line[19])/float(line[18])) + (float(line[13])/float(line[12])) + (float(line[28])/float(line[27]))/6)
-
-        if 'RDMF' in line[3]:
-            line[31] = ((float(line[6])/float(line[5])) + float(line[8]) + (float(line[10])/float(line[9])) + (
-                float(line[19])/float(line[18])) + (float(line[13])/float(line[12])) + (float(line[28])/float(line[27]))/6)
-
-        list.append(line)
-
-with open('Player_A_Diop2.csv', 'w', encoding="utf8", newline='') as f:
-    writer = csv.writer(f)
-    for line in list:
         writer.writerow(line)
+
+
+'''
+Things to keep in mind:
+
+The importance of different stats: Different stats may have different weights or values in determining a player's rating. For example, goals scored might be given more weight than completed passes.
+
+The context of the stats: The value of a player's stats may vary depending on the context in which they were achieved. For example, a player who scored many goals in a weak league might not be rated as highly as a player who scored fewer goals in a stronger league.
+
+The position of the player: Different positions may require different skills and stats, so a rating system may need to take into account the specific demands of each position.
+'''
